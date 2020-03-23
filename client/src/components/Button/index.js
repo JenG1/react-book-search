690df1477f4e0369425074f1ed0d1e5168c9d822
JGen1 <jengenes01@gmail.com>
2020-03-23 00:43:11 -0400

    Button css

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-function Button({ type = "default",children, click, value }) {
+function Button({ type = "default", children, click, value }) {
   return (
-      <button onClick={click} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" value={value}>
-      
+    //   <button onClick={click} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" value={value}>
+
+    //   {children}
+    // </button>
+    <button onClick={click} className="bg-transparent hover:bg-white text-blue-600 font-bold py-2 px-4 rounded" value={value}>
       {children}
     </button>
   );
